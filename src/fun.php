7e63a94a456f44ed63c07ee95ae851b5f298a0f6
azder <azhder@gmail.com>
2015-08-10 11:55:51 +0200

    updated the namespace and added more checks to the bool function

--- src/fun.php
+++ src/fun.php
@@ -1,15 +1,40 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: azder
- * Date: 10/08/2015
- * Time: 3:32
+ *CreatedbyPhpStorm.
+ *User:azder
+ *Date:10/08/2015
+ *Time:3:32
  */
 
 
-namespace Azder\Fun;
+namespace F;
 
+/**
+ * @param $value
+ *
+ * @return bool|null|string
+ */
 function bool( $value )
 {
-    return ! ! $value;
+
+    if (null === $value || false === $value || true === $value) {
+        return $value;
+    }
+
+    $value = trim( strtolower( $value ) );
+
+    if (in_array( $value, [ '', 'null', 'maybe' ] )) {
+        return null;
+    }
+
+    if (in_array( $value, [ 'no', 'off', '0' ] )) {
+        return false;
+    }
+
+    if (in_array( $value, [ 'yes', 'on', '1' ] )) {
+        return true;
+    }
+
+    return boolval( $value );
+
 }
