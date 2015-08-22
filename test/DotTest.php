e4de0d1e935b722a98c4cb41f166a50d987bfb1e
azder <azhder@gmail.com>
2015-08-22 07:55:16 +0200

    added DotTest

---
+++ test/DotTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azder
+ * Date: 22/08/2015
+ * Time: 6:45
+ */
+
+namespace F\Test;
+
+
+class DotTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_keyed_array()
+    {
+
+        $array = [
+            'first key'  => 1,
+            'second key' => new \stdClass(),
+            2            => 'third value',
+        ];
+
+        foreach ($array as $key => $value) {
+            $returned = \F\dot( $key, $array );
+            $this->assertSame( $value, $returned,
+                'dot(' . var_export( $key, true ) . ', ' . var_export( $array, true ) . ') '
+                . 'should return `'
+                . var_export( $value, true ) . '`, not ' . var_export( $returned, true ) . ''
+            );
+        }
+
+    }
+
+    public function test_indexed_array()
+    {
+
+        $array = [
+            'zero value',
+            new \stdClass(),
+            [ ]
+        ];
+
+        $aexpr = var_export( $array, true );
+        $count = count( $array );
+
+        for ($i = 0; $i < $count; $i += 1) {
+            $returned = \F\dot( $i, $array );
+            $rexp     = var_export( $returned, true );
+            $value    = $array[$i];
+            $vexp     = var_export( $value, true );
+            $this->assertSame( $value, $returned, "dot($i, $aexpr ) should return `$vexp`, not `$rexp`" );
+        }
+
+
+    }
+
+    public function test_mixed_array()
+    {
+
+        $k0 = 0;
+        $v0 = '0value';
+
+        $k1 = 'key1';
+        $v1 = 'key1value';
+
+        $k2 = 1;
+        $v2 = '1value';
+
+        $k3 = 'key2';
+        $v3 = 'key2value';
+
+        $array = [
+            $v0,
+            $k1 => $v1,
+            $v2,
+            $k3 => $v3,
+        ];
+
+        $assumed = [ 0 => '0value', 1 => '1value', 'key1' => 'key1value', 'key2' => 'key2value' ];
+
+        $this->assertEquals( $assumed, $array, 'test array not as assumed' );
+
+
+        $r0 = \F\dot( $k0, $array );
+        $this->assertSame( $v0, $r0, "value for `$k0` should be `$v0`, not `$r0`" );
+
+        $r1 = \F\dot( $k1, $array );
+        $this->assertSame( $v1, $r1, "value for `$k1` should be `$v1`, not `$r1`" );
+
+        $r2 = \F\dot( $k2, $array );
+        $this->assertSame( $v2, $r2, "value for `$k2` should be `$v2`, not `$r2`" );
+
+        $r3 = \F\dot( $k3, $array );
+        $this->assertSame( $v3, $r3, "value for `$k3` should be `$v3`, not `$r3`" );
+
+    }
+
+
+}
