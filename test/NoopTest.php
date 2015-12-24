59e1873c5b0bbb5adba81e6a403fd68f443311fa
azder <azhder@gmail.com>
2015-12-24 01:57:55 +0100

    updated NoopTest

--- test/NoopTest.php
+++ test/NoopTest.php
@@ -20,11 +20,14 @@ class NoopTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function test_null_return_with_parameters()
+    public function test_void_return_with_parameters()
     {
-        foreach ([ null, 1, true, 'asdf' ] as $value) {
+        foreach ([ null, 1, true, 'asdf', new \stdClass(), NAN, '', 0 ] as $value) {
             /** @noinspection PhpMethodParametersCountMismatchInspection */
-            $this->assertNull( \F\noop( $value ), 'noop() should return `null` for `' . $value . '`' );
+            $this->assertNull(
+                \F\noop( $value ),
+                'noop() should return `null` for `' . var_export( $value, true ) . '`'
+            );
         }
     }
 
